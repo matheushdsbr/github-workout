9c69f5b7ad454639d0cdbfd9d342521f7a64e639
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-17 19:26:37 -0300

    feat: add handleSearch function

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SearchContainer } from './styles';
 import InputField from '../Input';
 import StandardButton from '../Button';
 
-const Search = () => (
-  <div>
-    <SearchContainer>
-      <InputField placeholder="Search GitHub user..." name="username" />
-      <StandardButton type="button">Search</StandardButton>
-    </SearchContainer>
-  </div>
-);
+const Search = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <SearchContainer>
+        <InputField
+          placeholder="Search GitHub user..."
+          name="username"
+          onChange={handleSearch}
+        />
+        <StandardButton type="button">Search</StandardButton>
+        <p>{searchValue}</p>
+      </SearchContainer>
+    </div>
+  );
+};
 
 export default Search;
