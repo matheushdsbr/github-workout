601dba00d45da5c507cbce086a261ccfb07146db
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-17 19:23:43 -0300

    feat: add onChange props

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { InputField } from './styles';
 
-const Input = ({ placeholder, name }) => (
-  <InputField placeholder={placeholder} name={name} />
+const Input = ({ placeholder, name, onChange }) => (
+  <InputField placeholder={placeholder} name={name} onChange={onChange} />
 );
 
 export default Input;
