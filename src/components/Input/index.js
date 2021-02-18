602398c4edaa3e46813d06ab3e289db372bfc130
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-18 20:42:10 -0300

    refactor: remove the input component

--- src/components/Input/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { InputField } from './styles';
-
-const Input = ({ placeholder, name, onChange }) => (
-  <InputField placeholder={placeholder} name={name} onChange={onChange} />
-);
-
-export default Input;
