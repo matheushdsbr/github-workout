d7d8d1ddada972da80cd5961ff4955ffa49a9f52
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-18 20:48:54 -0300

    style: align components to the center of the screen

--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const SearchContainer = styled.div``;
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
