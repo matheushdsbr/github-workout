de088561f821b69d622d720f96a402989777ace7
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-18 23:43:38 -0300

    style: add style to resultrepos

--- src/components/ResultRepos/styles.js
+++ src/components/ResultRepos/styles.js
@@ -1,5 +1,39 @@
 import styled from 'styled-components';
 
-export const Text = styled.p``;
+export const ContainerResult = styled.li`
+  margin-bottom: 5px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #888888;
+`;
 
-export const Link = styled.a``;
+export const Title = styled.p`
+  color: #222222;
+  font-weight: 500;
+  margin-bottom: 6px;
+`;
+
+export const TextDescription = styled.p``;
+
+export const Link = styled.a`
+  color: #1fb5ff;
+  text-decoration: none;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 0;
+    height: 2px;
+    background: #1fb5ff;
+    transition: width 0.2s;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: #1fb5ff;
+    text-decoration: none;
+  }
+`;
