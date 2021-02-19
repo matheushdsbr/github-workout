e1be9a80c3d594b9c47d202d318a56012ff84438
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-18 22:56:32 -0300

    style: add styles to button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
 export const ButtonField = styled.button`
-  background: red;
+  background: white;
+  color: #222222;
+  font-weight: 500;
+  border: 1px solid #1fb5ff;
+  border-radius: 8px;
+  padding: 7px 25px;
+  cursor: pointer;
+  transition-duration: 0.5s;
+
+  &:focus {
+    background: #1fb5ff;
+    color: #ffffff;
+  }
 `;
