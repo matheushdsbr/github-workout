196ecf89eee5ae1cfe812b4057957fe6371f903c
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-19 12:28:00 -0300

    style: import media and adjust font-size on medium devices

--- src/components/Welcome/styles.js
+++ src/components/Welcome/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import FontSizes from '../../tokens/font-size';
 import colors from '../../tokens/colors';
+import media from '../../tokens/media';
 
 export const ContainerWelcome = styled.div`
   position: absolute;
@@ -16,9 +17,17 @@ export const ContainerWelcome = styled.div`
 export const TitleWelcome = styled.h1`
   color: ${colors.dark};
   font-size: ${FontSizes.xxlarge};
+
+  ${media.sm} {
+    font-size: ${FontSizes.xxxlarge};
+  }
 `;
 
 export const SubTitleWelcome = styled.p`
   font-size: ${FontSizes.large};
   color: ${colors.primary};
+
+  ${media.sm} {
+    font-size: ${FontSizes.xlarge};
+  }
 `;
