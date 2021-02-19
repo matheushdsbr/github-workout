a0ad1592a43b9dc5f06a70b3a5817e60267028d8
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-19 08:55:45 -0300

    style: adjust buttonfield padding, add media.md to change font-size

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import colors from '../../tokens/colors';
+import media from '../../tokens/media';
+import fontSizes from '../../tokens/font-size';
 
 export const ButtonField = styled.button`
   background: white;
@@ -7,9 +9,12 @@ export const ButtonField = styled.button`
   font-weight: 500;
   border: 1px solid ${colors.primary};
   border-radius: 8px;
-  padding: 7px 25px;
+  padding: 8px 14px;
   cursor: pointer;
   transition-duration: 0.5s;
+  ${media.md} {
+    font-size: ${fontSizes.large};
+  }
 
   &:focus {
     background: ${colors.primary};
