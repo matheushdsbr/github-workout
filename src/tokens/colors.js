d1b1c26ecc5a47bd2a4b7887285e316681b09316
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-19 07:22:33 -0300

    feat: add tokens: breakpoints, colors, media

---
+++ src/tokens/colors.js
@@ -0,0 +1,15 @@
+const white = '#FFFFFF';
+const dark = '#41414A';
+const gray = '#888888';
+const primary = '#1968C9';
+const negative = '#CB3530';
+
+const colors = {
+  white,
+  dark,
+  gray,
+  primary,
+  negative,
+};
+
+export default colors;
---
+++ src/tokens/index.js
@@ -0,0 +1,11 @@
+import breakpoints from './breakpoints';
+import colors from './colors';
+import media from './media';
+
+const tokens = {
+  breakpoints,
+  colors,
+  media,
+};
+
+export { tokens as default, breakpoints, colors, media };
