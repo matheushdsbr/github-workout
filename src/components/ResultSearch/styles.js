df161629cd206bb89185af16ab57acd3f9727584
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-18 22:26:41 -0300

    style: add styling to user data

--- src/components/ResultSearch/styles.js
+++ src/components/ResultSearch/styles.js
@@ -1,12 +1,54 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-top: 30px;
+`;
 
-export const Image = styled.img``;
+export const ContainerUser = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+`;
 
-export const Text = styled.p``;
+export const Image = styled.img`
+  width: 50%;
+  border-radius: 50%;
+  margin-bottom: 20px;
+`;
 
-export const Link = styled.a``;
+export const Title = styled.p`
+  color: #222;
+  font-weight: 500;
+`;
+
+export const Text = styled.p`
+  color: #888;
+`;
+
+export const Link = styled.a`
+  color: #1037c7;
+  text-decoration: none;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 0;
+    height: 2px;
+    background: #1037c7;
+    transition: width 0.2s;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: #1037c7;
+    text-decoration: none;
+  }
+`;
 
 export const ContainerButton = styled.div``;
 
