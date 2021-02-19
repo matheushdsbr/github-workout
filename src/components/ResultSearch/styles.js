aea458ddffe6283a40c2a71c249d2d37f632aea5
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-02-19 08:59:18 -0300

    style: add media.md to adjust font-size, and add 'containerList' to adjust the list in the center

--- src/components/ResultSearch/styles.js
+++ src/components/ResultSearch/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import colors from '../../tokens/colors';
 import fontSizes from '../../tokens/font-size';
+import media from '../../tokens/media';
 
 export const Container = styled.div`
   margin-top: 30px;
@@ -17,16 +18,27 @@ export const Image = styled.img`
   width: 50%;
   border-radius: 50%;
   margin-bottom: 20px;
+
+  ${media.md} {
+    width: 25%;
+  }
 `;
 
 export const Title = styled.p`
   color: ${colors.dark};
   font-weight: 500;
   font-size: ${fontSizes.large};
+
+  ${media.md} {
+    font-size: ${fontSizes.xlarge};
+  }
 `;
 
 export const Text = styled.p`
   color: ${colors.gray[1]};
+  ${media.md} {
+    font-size: ${fontSizes.large};
+  }
 `;
 
 export const Link = styled.a`
@@ -51,12 +63,24 @@ export const Link = styled.a`
     color: ${colors.primary};
     text-decoration: none;
   }
+
+  ${media.md} {
+    font-size: ${fontSizes.large};
+  }
 `;
 
 export const ContainerButton = styled.div`
   display: flex;
   justify-content: space-around;
   margin-bottom: 30px;
+  ${media.md} {
+    margin-bottom: 45px;
+  }
+`;
+
+export const ContainerList = styled.div`
+  width: 50%;
+  margin: auto;
 `;
 
 export const ListRepos = styled.ul`
